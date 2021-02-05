f260a9b22c252deb3f34a00b701141df778428c9
Lown Ignitus <lownignitus@gmail.com>
2021-02-05 15:19:31 -0700

    TOC Updates
    Updated for game version 9.0.2
    
    added X-Curse-Project-ID to toc to help WoWUp client

--- DemoSkinTracker.lua
+++ DemoSkinTracker.lua
@@ -1,6 +1,6 @@
 -- Title: Demonology Skin Tracker
 -- Author: LownIgnitus
--- Version: 1.0.8
+-- Version: 1.0.9
 -- Desc: Addon to track heads collected for demonology artifact skin
 
 CF = CreateFrame
